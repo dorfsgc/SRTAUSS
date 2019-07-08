030721f87969328b17ddd02ff381df9200c93c59
dorfsgc <50621877+dorfsgc@users.noreply.github.com>
2019-07-08 08:24:43 +0300

    0807

---
+++ pepper/story_content/user.js
@@ -0,0 +1,139 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6GWhcnM9pmi":
+        Script1();
+        break;
+      case "5rPfm2OOAHG":
+        Script2();
+        break;
+      case "6TRqcE8gBgc":
+        Script3();
+        break;
+      case "5a8yc7kzk8x":
+        Script4();
+        break;
+      case "5yCz9o9Hki0":
+        Script5();
+        break;
+      case "6X3HzMXXT0q":
+        Script6();
+        break;
+      case "5coFMqX9IbE":
+        Script7();
+        break;
+      case "6oSanVsSg49":
+        Script8();
+        break;
+      case "67xtS4J5vjE":
+        Script9();
+        break;
+      case "5g4c4u4FL6Q":
+        Script10();
+        break;
+      case "5WTW8Oa1R5D":
+        Script11();
+        break;
+      case "6V8bXoYtVJs":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var myvideo= player.video;
+myvideo.currentTime = 7;
+myvideo.play();
+}
+
